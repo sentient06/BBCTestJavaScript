97db4c5f3c21ed2eadf0971498a487fe68ffab2d
Giancarlo Mariot <gm@rockstardev.co>
2015-10-04 15:46:57 +0100

    Added HTML files, specs and controller file.

---
+++ jasmine/spec/MVCSpec.js
@@ -0,0 +1,3 @@
+describe("MVC", function() {
+
+});
---
+++ js/controller.js
@@ -0,0 +1,8 @@
+var IndexController = function(name) {
+    Controller.call(this, name);
+};
+
+
+IndexController.prototype = Object.create(Controller.prototype);
+IndexController.prototype.constructor = IndexController;
+
